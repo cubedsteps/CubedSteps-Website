7960f9e07ee7794bdea914c785a12f44a48ad8b0
Sohaildangol130 <sohail.dangol130@gmail.com>
2020-11-13 17:52:35 +0545

    created side-box transitions

--- js/body.js
+++ js/body.js
@@ -1,4 +1,4 @@
-//$("#body").loadTemplate("templates/homepage.html");
+$("#body").loadTemplate("templates/homepage.html");
 //$("#body").loadTemplate("templates/services.html");
-$("#body").loadTemplate("templates/lets-work.html");
+//$("#body").loadTemplate("templates/lets-work.html");
 //$("#body").loadTemplate("templates/our-projects.html");
--- js/left-side.js
+++ js/left-side.js
@@ -1,1 +1,9 @@
 $("#left-side").loadTemplate("templates/left-side.html")
+
+var testimonials = $(".side-box");
+console.log(testimonials);
+
+testimonials.forEach(click(function(){
+    
+}));
+
