dce14f19afde6aa7c500996dc0489020678d66c8
Sohaildangol130 <sohail.dangol130@gmail.com>
2020-12-02 17:20:39 +0545

    animation err

--- js/animations.js
+++ js/animations.js
@@ -1,29 +1,28 @@
 
 $(document).ready(function(){
-    var testimonial_box = $(".testimonial-box");
+    // var testimonial_box = $(".testimonial-box");
     var why_us_box = $(".why-us-box");   
 
-    $(document).on('click','.testimonial-box',function(){
-        // debugger;
-        for (var i = 0; i<testimonial_box.length; i++){
-            testimonial_box[i].setAttribute("data-clicked", "false");
-        }
-        // debugger;
-        $(this).attr("data-clicked","true");
-        for (var i = 0; i<testimonial_box.length; i++){
-            if(testimonial_box[i].getAttribute("data-clicked")==="true"){                
-                console.log("qwe")
-                $(testimonial_box[i]).toggleClass('testimonial-box-click');
-                $(testimonial_box[i]).find('.testimonial-content').toggleClass('testimonial-content-show');
-            }else{
-                console.log("asdasd")
-                $(testimonial_box[i]).removeClass('testimonial-box-click');
-                $(testimonial_box[i]).find('.testimonial-content').removeClass('testimonial-content-show');
-            }
-            // $(testimonial_box[i]).toggleClass('testimonial-box-click');
-            // $(testimonial_box[i]).find('.testimonial-content').toggleClass('testimonial-content-show');
-        }
-    })
+    // $(document).on('click','.testimonial-box',function(){
+    //     // debugger;
+    //     console.log('click');
+    //     for (var i = 0; i<testimonial_box.length; i++){
+    //         testimonial_box[i].setAttribute("data-clicked", "false");
+    //     }
+    //     // debugger;
+    //     $(this).attr("data-clicked","true");
+    //     for (var i = 0; i<testimonial_box.length; i++){
+    //         if(testimonial_box[i].getAttribute("data-clicked")==="true"){      
+    //             $(testimonial_box[i]).toggleClass('testimonial-box-click');
+    //             $(testimonial_box[i]).find('.testimonial-content').toggleClass('testimonial-content-show');
+    //         }else{
+    //             $(testimonial_box[i]).removeClass('testimonial-box-click');
+    //             $(testimonial_box[i]).find('.testimonial-content').removeClass('testimonial-content-show');
+    //         }
+    //         // $(testimonial_box[i]).toggleClass('testimonial-box-click');
+    //         // $(testimonial_box[i]).find('.testimonial-content').toggleClass('testimonial-content-show');
+    //     }
+    // })
 
     
 
@@ -88,7 +87,6 @@ $(document).ready(function(){
     }
     
     function navTrans(){
-        console.log($('.nav-items'));
         if($(".home-active").length>0){
             $(".nav-items").removeClass("home-active active");
         }
--- js/left-side.js
+++ js/left-side.js
@@ -1,3 +1,36 @@
-$("#left-side").loadTemplate("templates/left-side.html")
+// console.log("left-side");
+$("#left-side").loadTemplate("templates/left-side.html", {}, {
+    success: function(data){
+        console.log('success');
+        var testimonial_box = $(".testimonial-box");
+        var why_us_box = $(".why-us-box");   
+    
+        $(document).on('click','.testimonial-box',function(){
+            // debugger;
+            console.log('click');
+            for (var i = 0; i<testimonial_box.length; i++){
+                testimonial_box[i].setAttribute("data-clicked", "false");
+            }
+            // debugger;
+            $(this).attr("data-clicked","true");
+            for (var i = 0; i<testimonial_box.length; i++){
+                if(testimonial_box[i].getAttribute("data-clicked")==="true"){      
+                    $(testimonial_box[i]).toggleClass('testimonial-box-click');
+                    $(testimonial_box[i]).find('.testimonial-content').toggleClass('testimonial-content-show');
+                }else{
+                    $(testimonial_box[i]).removeClass('testimonial-box-click');
+                    $(testimonial_box[i]).find('.testimonial-content').removeClass('testimonial-content-show');
+                }
+                // $(testimonial_box[i]).toggleClass('testimonial-box-click');
+                // $(testimonial_box[i]).find('.testimonial-content').toggleClass('testimonial-content-show');
+            }
+        })
+    },
+    error: function(err) {
+        console.log('err');
+    }
+});
+// console.log($('*'));
+// console.log($(document).find('.testimonial_box'))
 
 
