5c1443a0ae356f475f1199a0d009e747af044c48
Avishek-45 <theabhi45@gmail.com>
2021-05-06 17:31:53 +0545

    add image_url, solve project_hover_issue

--- js/constant.js
+++ js/constant.js
@@ -21,6 +21,42 @@ var nav_contents = [{
 
 }]
 
+var project=[
+    // {
+    //     name: 'Circle',
+    //     description : 'Circle is an online platform for creating and sharing meaningful information and knowledge within the like minded community. The platform is for connecting learners and experts together and creating a positive influence in the community. ',
+    //     url : 'https://circleapp.insightjet.com',
+    //     backgroundImage : {"background-image":"linear-gradient(to bottom, rgba(0, 0, 0, 0), rgb(0, 0, 0)),url('https://res.cloudinary.com/cubedsteps/image/upload/v1620224464/cubedsteps-site-images/Circle_flrmqb.png')"}
+    // },
+    {
+        name: 'Repairmobile',
+        description : 'Repair Mobile helps users connect with the right mobile device repairer with better price in their locality. Users can choose the mobile brand and the seeking service and the platform shows them the mobile repairer in their vicinity. The platform also allows the customers to create online booking, compare prices with other service providers.',
+        url : 'https://repairmobile.com.au',
+        backgroundImage : {"background-image":"linear-gradient(to bottom, rgba(0, 0, 0, 0), rgb(0, 0, 0)),url('https://res.cloudinary.com/cubedsteps/image/upload/v1620224432/cubedsteps-site-images/repairmobile_j50g4t.png')"}
+    },
+    {
+        name: 'Jyatha',
+        description : 'Jyatha.com is the product of cubedsteps dedicated to the Nepali classified market.Jyatha.com is an ad posting platform where you could post ads of anything you would want to sell for free.',
+        url : 'https://jyatha.com/',
+        backgroundImage : {"background-image":"linear-gradient(to bottom, rgba(0, 0, 0, 0), rgb(0, 0, 0)),url('https://res.cloudinary.com/cubedsteps/image/upload/v1606818185/cubedsteps-site-images/Jyatha_mdpbli.png')"}
+    },
+    {
+        name: 'InsightJet',
+        description : 'Insight jet is a tool to collect reviews and ratings from your customer through email and sms. It provides a clear insight of what customers say about you and your business.',
+        url : 'https://insightjet.com/',
+        backgroundImage : {"background-image":"linear-gradient(to bottom, rgba(0, 0, 0, 0), rgb(0, 0, 0)),url('https://res.cloudinary.com/cubedsteps/image/upload/v1606818174/cubedsteps-site-images/InsightJet_ldafvh.png')"}
+        
+    },
+    {
+        name: 'Paxsafe',
+        description : 'Paxsafe is an online apartment booking tool used in australia. It has features like Booking management, guest email scheduling, seasonal rate set, payment integration and many more.',
+        url : 'https://paxsafe.com.au/',
+        backgroundImage : {"background-image":"linear-gradient(to bottom, rgba(0, 0, 0, 0), rgb(0, 0, 0)),url('https://res.cloudinary.com/cubedsteps/image/upload/v1606818199/cubedsteps-site-images/Paxsafe_na0t0v.png')"}
+    },
+
+]
+
+
 var services=[{
     services_name:"Product Development",
 },
