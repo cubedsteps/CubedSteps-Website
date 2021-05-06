8711cd508cabc60de1dbef0127e68b35a83fcd3c
Kiran <santakumal41@gmail.com>
2021-05-06 10:19:46 +0545

    adds the background image url and fix css

--- js/constant/classesConstant.js
+++ js/constant/classesConstant.js
@@ -1,17 +1,17 @@
 // constant for classes
 var classes = [{
-        backgroundImage: { "background-image": "linear-gradient(to bottom, rgba(0, 0, 0, 0), rgb(0, 0, 0)),url('https://d2908q01vomqb2.cloudfront.net/fc074d501302eb2b93e2554793fcaf50b3bf7291/2020/02/17/Architect-learning-path.png')" },
+        backgroundImage: { "background-image": "linear-gradient(to bottom, rgba(0, 0, 0, 0), rgb(0, 0, 0)),url('https://res.cloudinary.com/cubedsteps/image/upload/v1620271255/cubedsteps-site-images/AWS_Certified_Solutions_Architect_Associate_q2ppvo.png')" },
         name: "AWS Certified Solutions Architect Associate",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque quod amet perferendis consequatur recusandae reiciendis eligendi cupiditate, doloremque eveniet maiores ratione adipisci nam velit ducimus ipsa quas magni. Sit, repellendus."
+        description: "CubedSteps has been running training classes for one of the most sought after certifications in the IT world, AWS Certified Solutions Architect Associate. Our team consists of the instructors who are certified themselves and have excellent practical experience in the related field."
     },
     {
-        backgroundImage: { "background-image": "linear-gradient(to bottom, rgba(0, 0, 0, 0), rgb(0, 0, 0)),url('https://www.gangboard.com/wp-content/uploads/2019/07/Complete-Guide-To-AWS-Solution-Architect-Associate-Exam.jpg')" },
+        backgroundImage: { "background-image": "linear-gradient(to bottom, rgba(0, 0, 0, 0), rgb(0, 0, 0)),url('https://res.cloudinary.com/cubedsteps/image/upload/v1620271241/cubedsteps-site-images/AWS_Certified_Developer_Associate._lubs0k.png')" },
         name: "AWS Certified Developer Associate",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque quod amet perferendis consequatur recusandae reiciendis eligendi cupiditate, doloremque eveniet maiores ratione adipisci nam velit ducimus ipsa quas magni. Sit, repellendus."
+        description: "CubedSteps has been running training classes for one of the most sought after certifications in the IT world, AWS Certified Developer Associate. Our team consists of the instructors who are certified themselves and have excellent practical experience in the related field."
     },
     {
-        backgroundImage: { "background-image": "linear-gradient(to bottom, rgba(0, 0, 0, 0), rgb(0, 0, 0)),url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeBgEhifQVwYnRQpMMQFqOJh26oMuNRnnOow&usqp=CAU')" },
-        name: "Quality Assurance Training",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque quod amet perferendis consequatur recusandae reiciendis eligendi cupiditate, doloremque eveniet maiores ratione adipisci nam velit ducimus ipsa quas magni. Sit, repellendus.",
+        backgroundImage: { "background-image": "linear-gradient(to bottom, rgba(0, 0, 0, 0), rgb(0, 0, 0)),url('https://res.cloudinary.com/cubedsteps/image/upload/v1620271273/cubedsteps-site-images/QA_Training_k51r0q.png')" },
+        name: "QA Training",
+        description: "The principle objective of software testing is to give confidence in the software. Become a QA Ninja with our QA Training Course presented by Cubed Steps. We prepare you with different QA practices and processes. This is a job oriented course that will have students ready for applying for an internship and jobs after its completion.",
     }
 ];
